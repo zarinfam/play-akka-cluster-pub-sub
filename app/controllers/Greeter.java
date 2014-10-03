c6581486889bb0cf78b36f818c1a6476955b9b6a
zarinfam.s <zarinfam.s@gmail.com>
2014-10-03 15:34:26 +0330

    complete sample.

--- app/controllers/Greeter.java
+++ app/controllers/Greeter.java
@@ -5,45 +5,78 @@ import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.cluster.Cluster;
 import akka.cluster.ClusterEvent;
+import akka.cluster.Member;
 import play.Play;
+import play.PlayInternal;
 import play.libs.Akka;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Greeter extends UntypedActor {
 
-    static ActorRef defaultRoom = Akka.system().actorOf(Props.create(Greeter.class));
+    public static ActorRef actorRef = Akka.system().actorOf(Props.create(Greeter.class),"Greeter");
 
     Cluster cluster = Cluster.get(getContext().system());
+    List<Member> clusterMemberList = new ArrayList<Member>();
 
-    public static enum Msg {
-        GREET, DONE;
+    public static enum Msg implements Serializable{
+        GREET, DONE, NOTIFY_ALL;
     }
 
     public static void notifyAllNodes(){
-
+        PlayInternal.logger().info("---------------> notifyAllNodes");
+        actorRef.tell(Msg.NOTIFY_ALL, null);
     }
 
     //subscribe to cluster changes
     @Override
     public void preStart() {
         //#subscribe
-//        cluster.subscribe(getSelf(), ClusterEvent.initialStateAsEvents(),
-//                ClusterEvent.MemberEvent.class, UnreachableMember.class);
+        cluster.subscribe(getSelf(), ClusterEvent.initialStateAsEvents(),
+                ClusterEvent.MemberEvent.class, ClusterEvent.UnreachableMember.class);
         //#subscribe
     }
 
     //re-subscribe when restart
     @Override
     public void postStop() {
-//        cluster.unsubscribe(getSelf());
+        cluster.unsubscribe(getSelf());
     }
 
     @Override
-    public void onReceive(Object msg) {
-        if (msg == Msg.GREET) {
-            System.out.println("Hello from node " + Play.application().configuration().getString("node.id"));
+    public void onReceive(Object message) {
+        if (message == Msg.GREET) {
+            PlayInternal.logger().info("---------------> Hello from node " + Play.application().configuration().getString("node.id"));
 //      getSender().tell(Msg.DONE, getSelf());
-        } else
-            unhandled(msg);
+        }else if (message == Msg.NOTIFY_ALL){
+            PlayInternal.logger().info("---------------> start notify");
+            for(Member member: clusterMemberList){
+                PlayInternal.logger().info("---------------> notify = "+member.address());
+                getContext().actorSelection(member.address()+"/user/Greeter").tell(
+                        Greeter.Msg.GREET, getSelf());
+
+//                member.tell(Greeter.Msg.GREET, null);
+
+            }
+        }
+        else if (message instanceof ClusterEvent.MemberUp) {
+            ClusterEvent.MemberUp mUp = (ClusterEvent.MemberUp) message;
+            PlayInternal.logger().info("---------------> Member is Up: {}", mUp.member());
+            clusterMemberList.add(mUp.member());
+        } else if (message instanceof ClusterEvent.UnreachableMember) {
+            ClusterEvent.UnreachableMember mUnreachable = (ClusterEvent.UnreachableMember) message;
+            PlayInternal.logger().info("---------------> Member detected as unreachable: {}", mUnreachable.member());
+        } else if (message instanceof ClusterEvent.MemberRemoved) {
+            ClusterEvent.MemberRemoved mRemoved = (ClusterEvent.MemberRemoved) message;
+            PlayInternal.logger().info("---------------> Member is Removed: {}", mRemoved.member());
+            clusterMemberList.remove(mRemoved.member());
+        } else if (message instanceof ClusterEvent.MemberEvent) {
+            // ignore
+
+        }else
+            unhandled(message);
     }
 
 }
